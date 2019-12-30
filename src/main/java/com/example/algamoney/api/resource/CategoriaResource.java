05ed8317247d3571c5b74b0d6e3a2b4c0cbe1fc1
ellyzinha <adriellylovato@gmail.com>
2019-12-29 21:53:20 -0300

    Mudança de versão do spring boot para 1.5.3 e tratamento de erros com o exceptionHandler

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -3,10 +3,14 @@ package com.example.algamoney.api.resource;
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServletResponse;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -19,5 +23,23 @@ public class CategoriaResource {
     @GetMapping
     public List<Categoria> listar(){
         return categoriaRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
+        Categoria categoriaSalva =  categoriaRepository.save(categoria);
+        //Pegar a URI da requisição atual, adicionar o código e adicionar o código na URI.
+        URI uri =  ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
+        response.setHeader("Location:",uri.toASCIIString());
+
+        //Retorna o status Created e preenche o body
+        return ResponseEntity.created(uri).body(categoriaSalva);
+    }
+
+    //Requisição que pega a categoria pelo código na url
+    @GetMapping("/{codigo}")
+    public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo){
+        Categoria categoria = categoriaRepository.findOne(codigo);
+        return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
     }
 }
