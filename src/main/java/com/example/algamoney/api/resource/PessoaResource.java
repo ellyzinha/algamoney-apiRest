78367c2991d9d6c4906b1ddb2b08818e2d52ffc0
ellyzinha <adriellylovato@gmail.com>
2020-01-09 10:25:31 -0300

    Implementação do método atualizar parcialmente a propriedade ativo de Pessoa.

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -53,4 +53,11 @@ public class PessoaResource {
         Pessoa pessoaSalva = pessoaService.atualizar(codigo,pessoa);
         return ResponseEntity.ok(pessoaSalva);
     }
+
+    //Atualização parcial de uma propriedade
+    @PutMapping("/{codigo}/ativo")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo){
+         pessoaService.atualizarPropriedadeAtivo(codigo,ativo);
+    }
 }
