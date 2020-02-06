c51c29a8aea3d790ec474ad1f9123ea3c40d3857
ellyzinha <adriellylovato@gmail.com>
2020-02-05 21:04:42 -0300

    adicionando o método ResumoLancamento e o model de resumo de cada classe

--- src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,15 +1,15 @@
 package com.example.algamoney.api.repository.lancamento;
 
 import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.repository.projection.ResumoLancamento;
 import com.example.algamoney.api.repository.filter.LancamentoFilter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface LancamentoRepositoryQuery {
 
     public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
+    public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable);
 
 
 }
